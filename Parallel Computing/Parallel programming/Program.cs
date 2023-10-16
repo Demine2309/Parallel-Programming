f3c4c66e1319f405a0ff1272d7742ca7c2eb9146
Demine Nguyen <cuonghustmi1@gmail.com>
2023-10-16 23:37:52 +0700

    Demine
    16/10/2023

--- Parallel Computing/Parallel programming/Program.cs
+++ Parallel Computing/Parallel programming/Program.cs
@@ -902,39 +902,48 @@ namespace ParallelProgarmming
     #endregion
 
     #region Maximum Degree of Parallelism Example using Parallel Invoke method in C#
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var parallelOptions = new ParallelOptions()
-            {
-                MaxDegreeOfParallelism = 3
-                //MaxDegreeOfParallelism = Environment.ProcessorCount - 1
-            };
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        var parallelOptions = new ParallelOptions()
+    //        {
+    //            MaxDegreeOfParallelism = 3
+    //            //MaxDegreeOfParallelism = Environment.ProcessorCount - 1
+    //        };
 
-            //Passing ParallelOptions as the first parameter
-            Parallel.Invoke(
-                    parallelOptions,
-                    () => DoSomeTask(1),
-                    () => DoSomeTask(2),
-                    () => DoSomeTask(3),
-                    () => DoSomeTask(4),
-                    () => DoSomeTask(5),
-                    () => DoSomeTask(6),
-                    () => DoSomeTask(7)
-                );
+    //        //Passing ParallelOptions as the first parameter
+    //        Parallel.Invoke(
+    //                parallelOptions,
+    //                () => DoSomeTask(1),
+    //                () => DoSomeTask(2),
+    //                () => DoSomeTask(3),
+    //                () => DoSomeTask(4),
+    //                () => DoSomeTask(5),
+    //                () => DoSomeTask(6),
+    //                () => DoSomeTask(7)
+    //            );
 
-            Console.ReadLine();
-        }
+    //        Console.ReadLine();
+    //    }
 
-        static void DoSomeTask(int number)
-        {
-            Console.WriteLine($"DoSomeTask {number} started by Thread {Thread.CurrentThread.ManagedThreadId}");
-            //Sleep for 5000 milliseconds
-            Thread.Sleep(5000);
-            Console.WriteLine($"DoSomeTask {number} completed by Thread {Thread.CurrentThread.ManagedThreadId}");
-        }
-    }
+    //    static void DoSomeTask(int number)
+    //    {
+    //        Console.WriteLine($"DoSomeTask {number} started by Thread {Thread.CurrentThread.ManagedThreadId}");
+    //        //Sleep for 5000 milliseconds
+    //        Thread.Sleep(5000);
+    //        Console.WriteLine($"DoSomeTask {number} completed by Thread {Thread.CurrentThread.ManagedThreadId}");
+    //    }
+    //}
+    #endregion
+    #endregion
+
+    #region How to Cancel Parallel Operations in C#
+    #region Theory
+    /*
+     ** How to Cancel Parallel Operations in C#?
+     *  - 
+     */
     #endregion
     #endregion
 }
