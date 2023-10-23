020abe06c517878dd75f67f0772f69c364efbb81
Demine Nguyen <cuonghustmi1@gmail.com>
2023-10-23 10:49:08 +0700

    Demine
    23/10/2023

---
+++ Parallel Computing/Đề 13/Program.cs
@@ -0,0 +1,21 @@
+﻿#region Đề bài
+/*
+ * Cho tập tin input.txt, thực hiện việc đổi các ký tự chữ cái in thường thành in hoa nếu có và lưu vào tập tin output.txt.
+ */
+#endregion
+
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParallelComputing
+{
+
+
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
